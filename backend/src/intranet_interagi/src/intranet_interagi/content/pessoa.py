ff1d16a5360cb82c1eb588394ba0c4ec762a59df
renatorsdeoliveira <renatorsdeoliveira@gmail.com>
2023-05-12 20:56:19 -0300

    ex 24 - restrição ramaal e email

--- backend/src/intranet_interagi/src/intranet_interagi/content/pessoa.py
+++ backend/src/intranet_interagi/src/intranet_interagi/content/pessoa.py
@@ -6,6 +6,18 @@ from plone.supermodel.model import Schema
 from zope import schema
 from zope.interface import implementer
 
+import re
+
+
+def is_valid_email(value: str) -> bool:
+    """Validar se o email é @plone.org."""
+    return value.endswith("@plone.org") if value else True
+
+
+def is_valid_extension(value: str) -> bool:
+    """Validar se o o ramal tem 4 dígitos numéricos."""
+    return re.match(r"^\d{4}$", value) if value else True
+
 
 class IPessoa(Schema):
     """Uma Pessoa."""
@@ -24,8 +36,16 @@ class IPessoa(Schema):
     # Basic info
     title = schema.TextLine(title=_("Nome Completo"), required=True)
     description = schema.Text(title=_("Bio"), required=False)
-    email = Email(title=_("Email"), required=False)
-    ramal = schema.TextLine(title=("Ramal"), required=False)
+    email = Email(
+        title=_("Email"),
+        required=False,
+        constraint=is_valid_email,
+    )
+    ramal = schema.TextLine(
+        title=("Ramal"),
+        required=False,
+        constraint=is_valid_extension,
+    )
 
 
 @implementer(IPessoa)
