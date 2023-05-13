ab28091ee0cdac2b29245991e5c22f0da002d5b2
renatorsdeoliveira <renatorsdeoliveira@gmail.com>
2023-05-12 23:37:55 -0300

    Ex 26  - Remover duplicação de código

--- backend/src/intranet_interagi/src/intranet_interagi/content/pessoa.py
+++ backend/src/intranet_interagi/src/intranet_interagi/content/pessoa.py
@@ -1,22 +1,12 @@
 from intranet_interagi import _
+from intranet_interagi.validadores import is_valid_email
+from intranet_interagi.validadores import is_valid_extension
 from plone.dexterity.content import Container
 from plone.schema.email import Email
 from plone.supermodel import model
 from plone.supermodel.model import Schema
 from zope import schema
 from zope.interface import implementer
-
-import re
-
-
-def is_valid_email(value: str) -> bool:
-    """Validar se o email é @plone.org."""
-    return value.endswith("@plone.org") if value else True
-
-
-def is_valid_extension(value: str) -> bool:
-    """Validar se o o ramal tem 4 dígitos numéricos."""
-    return re.match(r"^\d{4}$", value) if value else True
 
 
 class IPessoa(Schema):
