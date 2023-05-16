1caed136ce3959cfddfe24fecbd10c960c7e6bfd
renatorsdeoliveira <renatorsdeoliveira@gmail.com>
2023-05-15 22:56:48 -0300

    EX 32 - Criar um grupo de usuários no Plone toda vez que uma nova Área for adicionada.

--- backend/src/intranet_interagi/tests/content/test_area.py
+++ backend/src/intranet_interagi/tests/content/test_area.py
@@ -133,3 +133,15 @@ class TestArea:
             notify(ObjectModifiedEvent(area))
 
         assert area.exclude_from_nav is False
+
+    def test_subscriber_added_creates_group(self, portal, payload):
+        from Products.PlonePAS.tools.groupdata import GroupData
+
+        with api.env.adopt_roles(["Manager"]):
+            area = api.content.create(container=portal, ** payload)
+        area_uuid = api.content.get_uuid(area)
+        group_name = f"{area_uuid}_editors"
+        group = api.group.get(groupname=group_name)
+        assert isinstance(group, GroupData)
+        roles = api.group.get_roles(group=group, obj=area)
+        assert "Editor" in roles
