a3a435a7bbd7a2d95a7cea56394d405097e6f548
renatorsdeoliveira <renatorsdeoliveira@gmail.com>
2023-05-21 11:55:17 -0300

    Ex 36 - Salvar o resultado da listagem em um arquivo json

--- scripts/listagem.py
+++ scripts/listagem.py
@@ -1,6 +1,7 @@
+from pathlib import Path
+import json
 import logging
 import requests
-
 
 # Define como faremos o log das ações
 logging.basicConfig()
@@ -9,9 +10,16 @@ logger.setLevel(logging.INFO)
 
 
 # Constantes utilizadas no script
+PASTA_ATUAL = Path(__file__).parent.resolve()
+PASTA_DADOS = PASTA_ATUAL / "data"
 BASE_URL="http://localhost:8080/Plone/++api++"
 USUARIO="admin"
 SENHA="admin"
+
+# Cria a pasta de dados caso ela não exista ainda
+if not PASTA_DADOS.exists():
+    PASTA_DADOS.mkdir(parents=True, exist_ok=True)
+    logger.info(f"Criada a pasta {PASTA_DADOS}")
 
 # Cabeçalhos HTTP
 headers = {
@@ -49,3 +57,10 @@ response = session.get(BASE_URL)
 data = response.json()
 total_conteudo = data["items_total"]
 logger.info(f"O portal conta com {total_conteudo} itens de conteúdo")
+
+
+# Salvar os dados recebidos do portal em um arquivo json
+arquivo_dados = PASTA_DADOS / "listagem.json"
+with open(arquivo_dados, "w") as fh:
+    json.dump(data, fh, indent=2)
+    logger.info(f"Dados da listagem salvos em {arquivo_dados}")
