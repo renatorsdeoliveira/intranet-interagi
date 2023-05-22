ef52d89998dc869efe95dd759df7ba8503a1f69b
renatorsdeoliveira <renatorsdeoliveira@gmail.com>
2023-05-22 12:43:35 -0300

    Ex 47.1 - Alterar AreaView para adicionar as informações de e-mail, ramal, predio

--- frontend/src/components/View/AreaView.jsx
+++ frontend/src/components/View/AreaView.jsx
@@ -5,6 +5,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Table } from 'semantic-ui-react';
 
 /**
  * AreaView view component class.
@@ -22,6 +23,40 @@ const AreaView = (props) => {
       </header>
       <div>
         <p className="description documentDescription">{content.description}</p>
+      </div>
+      <div>
+        <Table celled padded>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell singleLine>Contato</Table.HeaderCell>
+              <Table.HeaderCell></Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>E-mail</Table.Cell>
+              <Table.Cell singleLine>
+                {content.email ? (
+                  <a href={`mailto: ${content.email}`}>{content.email}</a>
+                ) : (
+                  ''
+                )}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Ramal</Table.Cell>
+              <Table.Cell singleLine>
+                {content.ramal ? content.ramal : '-'}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Prédio</Table.Cell>
+              <Table.Cell singleLine>
+                {content.predio ? content.predio.title : ''}
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
       </div>
     </div>
   );
