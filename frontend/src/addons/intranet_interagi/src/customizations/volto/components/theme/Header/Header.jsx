3a8cf6a331f151c6d9fa4a6050647e297c92d125
renatorsdeoliveira <renatorsdeoliveira@gmail.com>
2023-05-23 17:57:02 -0300

    EX 63 - Integrar o volto-bookmarks ao layout da Intranet

---
+++ frontend/src/addons/intranet_interagi/src/customizations/volto/components/theme/Header/Header.jsx
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import Container from '@kitconcept/volto-light-theme/components/Atoms/Container/Container';
+import { ShowBookmarksContentButton } from '@plone-collective/volto-bookmarks/components';
+import {
+  Anontools,
+  LanguageSelector,
+  Logo,
+  Navigation,
+  SearchWidget,
+} from '@plone/volto/components';
+
+const Header = (props) => {
+  const { pathname } = props;
+  const token = useSelector((state) => state.userSession.token);
+
+  return (
+    <header className="header-wrapper">
+      <Container layout>
+        <div className="header">
+          <div className="logo-nav-wrapper">
+            <div className="logo">
+              <Logo />
+            </div>
+            <Navigation pathname={pathname} />
+            <div className="search-wrapper">
+              <div className="search">
+                <SearchWidget />
+              </div>
+            </div>
+          </div>
+          <div className="tools-wrapper">
+            <LanguageSelector />
+            <div className="tools">
+              {!token && <Anontools />}
+              {token && <ShowBookmarksContentButton />}
+            </div>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+Header.propTypes = {
+  token: PropTypes.string,
+  pathname: PropTypes.string.isRequired,
+};
+
+Header.defaultProps = {
+  token: null,
+};
+
+export default Header;
