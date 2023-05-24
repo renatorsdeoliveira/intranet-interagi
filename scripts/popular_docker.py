1164c83650f9795c0fac0e37ad48c977a53816f7
renatorsdeoliveira <renatorsdeoliveira@gmail.com>
2023-05-24 16:16:41 -0300

    Ex 66.1

--- scripts/popular_docker.py
+++ scripts/popular_docker.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+
+import json
 import logging
 import requests
-from random import choice
+
 
 # Define como faremos o log das ações
 logging.basicConfig()
@@ -25,68 +27,20 @@ session = requests.Session()
 session.auth = (USUARIO, SENHA)
 session.headers.update(headers)
 
-conteudos = [
-    {
-        "_parent": "/",
-        "id": "time",
-        "@type": "Document",
-        "title": "Time",
-        "description": "Nossa Equipe",
-        "blocks": {
-            "a10e3d32-d2b5-4442-923b-e1e5e720948c": {
-            "@type": "title"
-            },
-            "52eb0f0d-6131-4696-a794-510bd086ebac": {
-            "@type": "listing"
-            }
-        },
-        "blocks_layout": {
-            "items": [
-            "a10e3d32-d2b5-4442-923b-e1e5e720948c",
-            "52eb0f0d-6131-4696-a794-510bd086ebac"
-            ]
-        },
-        "language": "pt-br",
-        "subjects": [
-            "Time",
-            "RH"
-        ]
-    },
-    {
-        "_parent": "/",
-        "id": "ti",
-        "@type": "Area",
-        "title": "TI",
-        "description": "Tecnologia da Informação",
-        "email": "ti@plone.org",
-        "ramal": "1874",
-    },
-    {
-        "_parent": "/ti/",
-        "id": "desenvolvimento",
-        "@type": "Area",
-        "title": "Desenvolvimento",
-        "description": "Tecnologia da Informação: Time de Desenvolvimento",
-        "email": "dev@plone.org",
-        "ramal": "1337",
-    },
-    {
-        "_parent": "/",
-        "id": "marketing",
-        "@type": "Area",
-        "title": "Marketing",
-        "description": "Marketing",
-        "email": "mktg@plone.org",
-        "ramal": "3322",
-    },
-]
+with open(PASTA_ATUAL / "data" / "conteudos.json", "r") as fh:
+    conteudos = json.load(fh)
+
 for conteudo in conteudos:
     parent = conteudo["_parent"]
     o_id = conteudo["id"]
     del conteudo["_parent"]
     parent_url = f"{BASE_URL}{parent}"
-    response = session.get(f"{parent_url}{o_id}")
+    content_url = f"{parent_url}{o_id}"
+    response = session.get(content_url)
     if response.status_code == 404:
         response = session.post(parent_url, json=conteudo)
         content = response.json()
         logger.info(f"Conteúdo {content['@id']} criado")
+    elif response.status_code == 200:
+        response = session.patch(content_url, json=conteudo)
+        logger.info(f"Conteúdo {content_url} atualizado")
